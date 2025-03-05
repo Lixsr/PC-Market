836c22c982eb5348763609ac74b5b6678485a9c9
Lixsr <osama142494@gmail.com>
2025-03-05 17:35:32 +0300

    get cart

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,5 +1,63 @@
 "use server";
+
+import { cookies } from "next/headers";
 import { CartItem } from "@/types";
+import { formatError, toPlainObject } from "../utils";
+import { auth } from "@/auth";
+import { prisma } from "@/db/prisma";
+import { cartItemSchema } from "../validators";
+
 export const addToCart = async (item: CartItem) => {
-  return { success: true, message: "Item added to cart" };
+  try {
+    const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+    // handle missing sessionCartId
+    if (!sessionCartId) {
+      throw new Error("Session Cart Id not found");
+    }
+    // Get the user id from the session
+    const session = await auth();
+    const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+    // Get user cart
+    const cart = await getCart();
+    const requestedItem = cartItemSchema.parse(item);
+
+    // Fetch the item from the DB
+    const product = await prisma.product.findFirst({
+      where: { id: requestedItem.productId },
+    });
+    // temporary log
+    console.log({
+      sessionCartId: sessionCartId,
+      userId: userId,
+      item: requestedItem,
+      product: product,
+    });
+    return { success: true, message: "Item added to cart" };
+  } catch (e) {
+    return { success: false, message: formatError(e) };
+  }
 };
+export async function getCart() {
+  const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+  // handle missing sessionCartId
+  if (!sessionCartId) {
+    throw new Error("Session Cart Id not found");
+  }
+  // Get the user id from the session
+  const session = await auth();
+  const userId = session?.user?.id ? (session.user.id as string) : undefined;
+  // Get user cart from DB by id or sessionCartId
+  const cart = await prisma.cart.findFirst({
+    where: userId ? { userId } : { sessionCartId },
+  });
+  if (!cart) return undefined;
+  return toPlainObject({
+    ...cart,
+    items: cart.items as CartItem[],
+    itemsPrice: cart.itemsPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
+    shippingPrice: cart.shippingPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
+  });
+}
