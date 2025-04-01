1c09484aeef66db6fefe466397228f7c42e72459
Lixsr <osama142494@gmail.com>
2025-04-01 13:43:25 +0300

    paypal setup

---
+++ lib/paypal.ts
@@ -0,0 +1,24 @@
+const baseUrl =
+  process.env.PAYPAL_API_URL || "https://api-m.sandbox.paypal.com";
+
+export const paypal = {};
+
+// Paypal Token
+async function generatePaypalToken() {
+  const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
+  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString(
+    "base64"
+  );
+  const response = await fetch(`${baseUrl}/v1/oauth2/token`, {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${auth}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: "grant_type=client_credentials",
+  });
+  if (response.ok) {
+    const { access_token } = await response.json();
+    return access_token;
+  } else throw new Error(await response.text());
+}
