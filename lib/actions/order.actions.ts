972619de05c2286a054539ca18204795540d6fd3
Lixsr <osama142494@gmail.com>
2025-04-04 14:47:43 +0300

    Cash on Delivery update functions

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -297,3 +297,68 @@ export async function deleteOrder(orderId: string) {
     return { success: false, message: formatError(error) };
   }
 }
+
+// Update order to delivered
+export async function deliverOrder(orderId: string) {
+  try {
+    const order = await prisma.order.findFirst({
+      where: {
+        id: orderId,
+      },
+    });
+
+    if (!order) throw new Error("Order not found");
+    if (!order.isPaid) throw new Error("Order is not paid");
+    if (order.isDelivered) throw new Error("Order is already delivered");
+
+    await prisma.order.update({
+      where: { id: orderId },
+      data: {
+        isDelivered: true,
+        deliveredAt: new Date(),
+      },
+    });
+
+    revalidatePath(`/order/${orderId}`);
+
+    return { success: true, message: "Order is marked as delivered" };
+  } catch (err) {
+    return { success: false, message: formatError(err) };
+  }
+}
+
+// Update order to paid for Cash on Delivery
+export async function updateOrderToPaidCOD(orderId: string) {
+  try {
+    const order = await prisma.order.findFirst({
+      where: { id: orderId },
+      include: { orderItems: true },
+    });
+    if (!order) throw new Error("Order not found");
+    if (order.isPaid) throw new Error("Order is already paid");
+    await prisma.$transaction(async (tx) => {
+      // Update stock
+      for (const item of order.orderItems) {
+        await tx.product.update({
+          where: { id: item.productId },
+          data: { stock: { decrement: item.quantity } },
+        });
+      }
+      // Update order to paid
+      await tx.order.update({
+        where: { id: orderId },
+        data: {
+          isPaid: true,
+          paidAt: new Date(),
+        },
+      });
+    });
+    revalidatePath(`/order/${orderId}`);
+    return {
+      success: true,
+      message: "Order is marked as paid",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
