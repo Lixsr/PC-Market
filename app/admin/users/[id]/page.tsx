40f655bb4715195f12ea40089cd32d5ae195a6c7
Lixsr <osama142494@gmail.com>
2025-04-06 20:42:57 +0300

    Admin | Update User page

---
+++ app/admin/users/[id]/page.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { getUser } from "@/lib/actions/user.actions";
+
+export const metadata: Metadata = {
+  title: "Update user",
+};
+
+const UpdateUserPage = async (props: {
+  params: Promise<{
+    id: string;
+  }>;
+}) => {
+  const { id } = await props.params;
+  const user = await getUser(id);
+  if (!user) notFound();
+
+  return (
+    <div className="space-y-8 max-w-lg mx-auto">
+      <h2 className="h2-bold">Update User</h2>
+    </div>
+  );
+};
+
+export default UpdateUserPage;
