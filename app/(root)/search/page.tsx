f0e1f671ca852f72abae84a663c23999be1f0c27
Lixsr <osama142494@gmail.com>
2025-04-07 18:13:46 +0300

    construct filterURL

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -24,7 +24,29 @@ const SearchPage = async (props: {
     sort = "recent",
     page = "1",
   } = await props.searchParams;
-  const {products, totalPages} = await getAllProducts({
+
+  const getFilterUrl = ({
+    c,
+    s,
+    p,
+    r,
+    pg,
+  }: {
+    c?: string;
+    s?: string;
+    p?: string;
+    r?: string;
+    pg?: string;
+  }) => {
+    const params = { query, category, price, rating, sort, page };
+    if (c) params.category = c;
+    if (p) params.price = p;
+    if (r) params.rating = r;
+    if (pg) params.page = pg;
+    if (s) params.sort = s;
+    return `/search?${new URLSearchParams(params).toString()}`;
+  };
+  const { products, totalPages } = await getAllProducts({
     query,
     category,
     price,
@@ -34,7 +56,9 @@ const SearchPage = async (props: {
   });
   return (
     <div className="grid md:grid-cols-5 md:gap-5">
-      <div className="filter-links">{/* FILTERS */}</div>
+      <div className="filter-links">
+        {/* ToDo: construct URL */}
+      </div>
       <div className="md:col-span-4 space-y-4">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
           {products?.length === 0 && <div>No product found</div>}
@@ -42,9 +66,8 @@ const SearchPage = async (props: {
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
-        {totalPages! > 1 && (
-          <Pagination page={page} totalPages={totalPages} />
-        )}
+        
+        {totalPages! > 1 && <Pagination page={page} totalPages={totalPages} />}
       </div>
     </div>
   );
