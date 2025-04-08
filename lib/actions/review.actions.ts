f5e19f0e22aa27468a6d15707fc504443bb02071
Lixsr <osama142494@gmail.com>
2025-04-08 20:53:04 +0300

    Review action

---
+++ lib/actions/review.actions.ts
@@ -0,0 +1,87 @@
+"use server";
+import { revalidatePath } from "next/cache";
+import { z } from "zod";
+import { auth } from "@/auth";
+import { formatError } from "../utils";
+import { insertReviewSchema } from "../validators";
+import { prisma } from "@/db/prisma";
+
+export async function publishReview(
+    data: z.infer<typeof insertReviewSchema>
+  ) {
+    try {
+      const session = await auth();
+      if (!session) throw new Error('User is not authenticated');
+  
+      // Validate and store review data and userId
+      const review = insertReviewSchema.parse({
+        ...data,
+        userId: session?.user.id,
+      });
+  
+      // Get the product being reviewed
+      const product = await prisma.product.findFirst({
+        where: { id: review.productId },
+      });
+  
+      if (!product) throw new Error('Product not found');
+  
+      // Check if user has already reviewed this product
+      const foundReview = await prisma.review.findFirst({
+        where: {
+          productId: review.productId,
+          userId: review.userId,
+        },
+      });
+  
+      // If review exists, update it, otherwise create a new one
+      await prisma.$transaction(async (tx) => {
+        if (foundReview) {
+          // Update the review
+          await tx.review.update({
+            where: { id: foundReview.id },
+            data: {
+              description: review.description,
+              title: review.title,
+              rating: review.rating,
+            },
+          });
+        } else {
+          // Create a new review
+          await tx.review.create({ data: review });
+        }
+  
+        // Get the average rating
+        const averageRating = await tx.review.aggregate({
+          _avg: { rating: true },
+          where: { productId: review.productId },
+        });
+  
+        // Get the number of reviews
+        const totalReviews = await tx.review.count({
+          where: { productId: review.productId },
+        });
+  
+        // Update rating and  number of reviews
+        await tx.product.update({
+          where: { id: review.productId },
+          data: {
+            rating: averageRating._avg.rating || 0,
+            numReviews: totalReviews,
+          },
+        });
+      });
+  
+      revalidatePath(`/product/${product.slug}`);
+  
+      return {
+        success: true,
+        message: 'Review published successfully',
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: formatError(error),
+      };
+    }
+  }
